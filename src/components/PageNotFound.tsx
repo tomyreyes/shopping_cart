4262e54d3fe36b627a531a8747fc9382f1f69ce0
Tomy <tomyleereyes@gmail.com>
2020-12-11 19:58:00 -0800

    Issue #3 handle errors

---
+++ src/components/PageNotFound.tsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export const PageNotFound = (): JSX.Element => (
+    <div>Page not found. </div>
+);
--- src/components/main/PageSwitcher.tsx
+++ src/components/main/PageSwitcher.tsx
@@ -5,15 +5,17 @@ import { Categories } from '../Categories';
 import { ProductsList } from '../ProductsList';
 import { ProductDetails } from '../ProductDetails';
 import { Cart } from '../Cart';
+import { PageNotFound } from '../PageNotFound';
 
 export const PageSwitcher = (): JSX.Element => {
     return (
         <Switch>
             <Route exact path='/' render={Home} />
             <Route exact path='/categories' render={Categories} />
-            <Route exact path='categories/:productListId' render={ProductsList} />
-            <Route exact pathj='product/productId' render={ProductDetails}/>
+            <Route exact path='/categories/:productListId' render={ProductsList} />
+            <Route exact path='/product/:productId' render={ProductDetails}/>
             <Route exact path='/cart' render={Cart} />
+            <Route component={PageNotFound} />
         </Switch>
     );
 };
