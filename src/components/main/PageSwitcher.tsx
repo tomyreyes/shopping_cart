d270083d71e64768d77cfdc0caa0955b3b46fc5d
Tomy <tomyleereyes@gmail.com>
2020-12-11 19:55:11 -0800

    Issue #3 add products list and product detail page

--- src/components/main/PageSwitcher.tsx
+++ src/components/main/PageSwitcher.tsx
@@ -2,6 +2,8 @@ import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 import { Cart } from '../Cart';
 import { Categories } from '../Categories';
+import { ProductDetails } from '../ProductDetails';
+import { ProductsList } from '../ProductsList';
 import { Home } from './Home';
 
 export const PageSwitcher = (): JSX.Element => {
@@ -9,6 +11,8 @@ export const PageSwitcher = (): JSX.Element => {
         <Switch>
             <Route exact path='/' render={Home} />
             <Route exact path='/categories' render={Categories} />
+            <Route exact path='categories/:productListId' render={ProductsList} />
+            <Route exact pathj='product/productId' render={ProductDetails}/>
             <Route exact path='/cart' render={Cart} />
         </Switch>
     );
