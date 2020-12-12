633c8decc6ae126044e3254e08d4910788605a0d
Tomy <tomyleereyes@gmail.com>
2020-12-11 19:48:23 -0800

    Issue #3 add categories page

---
+++ src/components/Categories.tsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export const Categories = (): JSX.Element => (
+    <div>This is the Categories page. This will show all of the categories</div>
+);
--- src/components/main/PageSwitcher.tsx
+++ src/components/main/PageSwitcher.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 import { Cart } from '../Cart';
+import { Categories } from '../Categories';
 import { Home } from './Home';
 
 export const PageSwitcher = (): JSX.Element => {
     return (
         <Switch>
             <Route exact path='/' render={Home} />
+            <Route exact path='/categories' render={Categories} />
             <Route exact path='/cart' render={Cart} />
         </Switch>
     );
