299b54543195eabb6942adaf411daf711a741c93
Tomy <tomyleereyes@gmail.com>
2020-12-13 14:36:34 -0800

    Issue #11 prepare for dispatching load

--- src/application/index.tsx
+++ src/application/index.tsx
@@ -1,16 +1,19 @@
+// tslint:disable: no-expression-statement
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { Footer } from '../components/main/Footer';
 import { Header } from '../components/main/Header';
 import { PageSwitcher } from '../components/main/PageSwitcher';
-import { buildSaga, runSaga } from '../sagas';
+import { ApplicationSaga, buildSaga, runSaga } from '../sagas';
 import { buildStore } from '../application/store';
 
 const saga = buildSaga();
 const store = buildStore(saga);
-// tslint:disable-next-line: no-expression-statement
-runSaga(saga.middleware);
+const startApplication = (applicationSaga: ApplicationSaga): void => {
+    runSaga(applicationSaga.middleware);
+};
+startApplication(saga);
 
 export const Application = (): JSX.Element => {
     return (
