8f30f157d76da8851ec03d42623e2a0158e368c7
Tomy <tomyleereyes@gmail.com>
2020-12-13 10:58:52 -0800

    Issue #9 fix test

--- src/stores/__tests__/cart.test.ts
+++ src/stores/__tests__/cart.test.ts
@@ -1,30 +1,50 @@
+import { aNumber, aString } from '../../application/helpers/randomTestValues';
 import { buildDefaultStore, reducer } from '../cart';
 import { addItem, removeItem } from '../cart/actions';
+import { Item } from '../items/types';
 
 // tslint:disable:no-expression-statement typedef
 describe('the store reducer', () => {
     describe('the items state', () => {
         it('adds a item to the items list by the add item action', () => {
             const oldStore = buildDefaultStore();
-            const item = {
-                id: '1',
-                title: 'First item in cart',
+            const item: Item = {
+                id: aNumber(),
+                title: aString(),
+                description: aString(),
+                price: aNumber(),
+                views: aNumber(),
+                imageProperties: {
+                    avatarUrl: aString(),
+                    smallUrl: aString(),
+                    mediumUrl: aString(),
+                    fullUrl: aString(),
+                },
             };
             const newStore = reducer(oldStore, addItem(item));
             expect(newStore.items[0]).toBe(item);
         });
 
         it('removes an item from the items list by the remove item action', () => {
+            const item: Item = {
+                id: aNumber(),
+                title: aString(),
+                description: aString(),
+                price: aNumber(),
+                views: aNumber(),
+                imageProperties: {
+                    avatarUrl: aString(),
+                    smallUrl: aString(),
+                    mediumUrl: aString(),
+                    fullUrl: aString(),
+                },
+            };
             const oldStore = {
                 ...buildDefaultStore(),
-                items: [{
-                    id: '1',
-                    title: 'First item in cart',
-                }],
+                items: [item],
                 cost: 0,
             };
-            const itemId = '1';
-            const newStore = reducer(oldStore, removeItem(itemId));
+            const newStore = reducer(oldStore, removeItem(item.id));
             expect(newStore.items.length).toBe(0);
         });
     });
