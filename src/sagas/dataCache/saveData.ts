bc8c95fa13ff8ab96f2936f5e8bd14ccafaa2880
Tomy <tomyleereyes@gmail.com>
2020-12-13 15:13:15 -0800

    Issue #11 add saga for loading cached data

--- src/sagas/dataCache/saveData.ts
+++ src/sagas/dataCache/saveData.ts
@@ -1,8 +1,8 @@
 // tslint:disable: no-expression-statement
-import * as constants from '../application/constants';
+import * as constants from '../../application/constants';
 import { ForkEffect, select, takeLatest, call, put } from 'redux-saga/effects';
-import { selectDataForCache } from '../selectors/cache/selectDataForCache';
-import { DataCache, saveDataError, saveDataSuccess } from '../stores/dataCache';
+import { selectDataForCache } from '../../selectors/cache/selectDataForCache';
+import { DataCache, saveDataError, saveDataSuccess } from '../../stores/dataCache';
 
 export function* watchStateChangesToSaveToCache(): IterableIterator<ForkEffect> {
     yield takeLatest(constants.REQUEST_ITEMS_BY_CATEGORY_SUCCESS, saveDataToCache);
--- src/stores/dataCache.ts
+++ src/stores/dataCache.ts
@@ -13,10 +13,36 @@ export interface SaveDataErrorAction {
     readonly type: typeof constants.SAVE_DATA_ERROR;
 }
 
+export interface LoadCachedDataRequestAction {
+    readonly type: typeof constants.LOAD_CACHED_DATA_REQUEST;
+}
+
+export interface LoadCachedDataSuccessAction {
+    readonly type: typeof constants.LOAD_CACHED_DATA_SUCCESS;
+    readonly payload: DataCache;
+}
+
+export interface LoadCachedDataErrorAction {
+    readonly type: typeof constants.LOAD_CACHED_DATA_ERROR;
+}
+
 export const saveDataSuccess = (): SaveDataSuccessAction => ({
     type: constants.SAVE_DATA_SUCCESS,
 });
 
 export const saveDataError = (): SaveDataErrorAction => ({
     type: constants.SAVE_DATA_ERROR,
+});
+
+export const loadCachedDataRequest = (): LoadCachedDataRequestAction => ({
+    type: constants.LOAD_CACHED_DATA_REQUEST,
+});
+
+export const loadCachedDataSuccess = (dataCache: DataCache): LoadCachedDataSuccessAction => ({
+    type: constants.LOAD_CACHED_DATA_SUCCESS,
+    payload: dataCache,
+});
+
+export const loadCachedDataError = (): LoadCachedDataErrorAction => ({
+    type: constants.LOAD_CACHED_DATA_ERROR,
 });
