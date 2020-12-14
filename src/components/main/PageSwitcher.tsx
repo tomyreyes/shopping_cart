a757cdbbf315ea28bd8d91889728a36b91f9ac90
Tomy <tomyleereyes@gmail.com>
2020-12-13 23:09:37 -0800

    Issue #17 remove no longer needed component

--- src/components/main/PageSwitcher.tsx
+++ src/components/main/PageSwitcher.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 import { HomeConnected } from '../Home/HomeConnected';
 import { ProductsListConnected } from '../ProductsList/ProductsListConnected';
-import { ProductDetails } from '../ProductDetails';
 import { CartConnected } from '../Cart/CartConnected';
 import { PageNotFound } from '../PageNotFound';
 
@@ -11,7 +10,6 @@ export const PageSwitcher = (): JSX.Element => {
         <Switch>
             <Route exact path='/' component={HomeConnected} />
             <Route exact path='/categories/:categoryId' component={ProductsListConnected} />
-            <Route exact path='/product/:productId' render={ProductDetails}/>
             <Route exact path='/cart' component={CartConnected} />
             <Route component={PageNotFound} />
         </Switch>
