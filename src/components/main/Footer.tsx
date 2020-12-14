0abd28491f11a2e0d2fc32bb7d0c2ac7bfeb75dd
Tomy <tomyleereyes@gmail.com>
2020-12-13 18:17:21 -0800

    Issue #16 add footer

--- src/components/main/Footer.tsx
+++ src/components/main/Footer.tsx
@@ -1,5 +1,10 @@
 import React from 'react';
+import { Typography } from '@material-ui/core';
 
 export const Footer = (): JSX.Element => (
-    <div>Copyright 2020</div>
+    <footer>
+        <Typography variant='h6' align='center' gutterBottom>
+            Handmade Fandom
+        </Typography>
+    </footer>
 );
