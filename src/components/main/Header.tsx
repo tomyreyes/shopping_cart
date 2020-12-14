4409c77155e594940de980be2f73d3e483683f97
Tomy <tomyleereyes@gmail.com>
2020-12-14 00:15:13 -0800

    Issue #20 add shopping cart icon

--- src/components/main/Header.tsx
+++ src/components/main/Header.tsx
@@ -1,18 +1,24 @@
 import React from 'react';
-import { AppBar, Toolbar, Typography, MenuItem } from '@material-ui/core';
+import { AppBar, Toolbar, Typography, MenuItem, IconButton, Badge } from '@material-ui/core';
 import { useStyles } from '../styles/useStyles';
 import { Link } from 'react-router-dom';
+import ShoppingCartIcon from '@material-ui/icons/ShoppingCart';
 
 export const Header = (): JSX.Element => {
     const classes = useStyles();
     return (
         <AppBar position='relative' className={classes.header}>
             <Toolbar>
-                <MenuItem component={Link} to='/'>
+                <MenuItem component={Link} to='/' style={{ flex: 1}}>
                     <Typography variant='h6' noWrap>
                         Handmade Fandom
                     </Typography>
                 </MenuItem>
+                <IconButton className={classes.cartIcon}>
+                    <Badge badgeContent={3} color='primary'>
+                    <ShoppingCartIcon />
+                    </Badge>
+                </IconButton>
             </Toolbar>
         </AppBar>
     );
