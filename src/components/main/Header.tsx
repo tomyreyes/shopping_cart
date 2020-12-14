04a3e591b42dd081ec2cdade6f3aafe7c5ed7427
Tomy <tomyleereyes@gmail.com>
2020-12-13 23:50:10 -0800

    Issue #20 navigate back to home

--- src/components/main/Header.tsx
+++ src/components/main/Header.tsx
@@ -1,15 +1,23 @@
 import React from 'react';
-import { AppBar, Toolbar, Typography } from '@material-ui/core';
+import { AppBar, Button, Toolbar, Typography } from '@material-ui/core';
 import { useStyles } from '../styles/useStyles';
+import { useHistory } from 'react-router-dom';
 
 export const Header = (): JSX.Element => {
+    const history = useHistory();
     const classes = useStyles();
+    const onClick = (): void => {
+        // tslint:disable-next-line: no-expression-statement
+        history.push('/')
+    };
     return (
         <AppBar position='relative' className={classes.header}>
             <Toolbar>
-                <Typography variant='h6' noWrap>
-                    Handmade Fandom
-            </Typography>
+                <Button onClick={onClick}>
+                    <Typography variant='h6' noWrap>
+                        Handmade Fandom
+                    </Typography>
+                </Button>
             </Toolbar>
         </AppBar>
     );
