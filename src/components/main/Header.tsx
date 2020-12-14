f6810c9bdedb92e77ff44bc87e59f48215cd7ece
Tomy <tomyleereyes@gmail.com>
2020-12-13 23:57:16 -0800

    Issue #20 improve navigation to home screen

--- src/components/main/Header.tsx
+++ src/components/main/Header.tsx
@@ -1,23 +1,18 @@
 import React from 'react';
-import { AppBar, Button, Toolbar, Typography } from '@material-ui/core';
+import { AppBar, Toolbar, Typography, MenuItem } from '@material-ui/core';
 import { useStyles } from '../styles/useStyles';
-import { useHistory } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 export const Header = (): JSX.Element => {
-    const history = useHistory();
     const classes = useStyles();
-    const onClick = (): void => {
-        // tslint:disable-next-line: no-expression-statement
-        history.push('/')
-    };
     return (
         <AppBar position='relative' className={classes.header}>
             <Toolbar>
-                <Button onClick={onClick}>
+                <MenuItem component={Link} to='/'>
                     <Typography variant='h6' noWrap>
                         Handmade Fandom
                     </Typography>
-                </Button>
+                </MenuItem>
             </Toolbar>
         </AppBar>
     );
